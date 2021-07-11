d2b3d98273a05946feb5758708e079b07cfebb82
Ryan <draco124@hotmail.com>
2021-07-10 20:29:07 -0700

    1

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+};
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <Header />
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  );
+}
