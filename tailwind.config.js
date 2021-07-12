0e2f1df9490d45792d098304d446c47f0b5b6c84
Ryan <draco124@hotmail.com>
2021-07-11 21:11:17 -0700

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"],
+      borderColor: ["active"],
+    },
   },
   plugins: [],
 };
